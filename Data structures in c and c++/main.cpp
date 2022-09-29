032e5cc406b6180caf1fee415be25f2b9c4dc9bf
skm5786 <sumitthakral3@gmail.com>
2022-09-29 20:36:41 +0530

    Initial Commit

---
+++ Data structures in c and c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data structures in c and c++
+//
+//  Created by sumit thakral on 29/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
