ab3acf67bc7e2447c77fb440cefeb95a0c69c0d5
skm5786 <sumitthakral3@gmail.com>
2022-10-04 09:02:11 +0530

    commit

--- Data structures in c and c++/main.cpp
+++ Data structures in c and c++/main.cpp
@@ -1,14 +1,56 @@
-//
-//  main.cpp
-//  Data structures in c and c++
-//
-//  Created by sumit thakral on 29/09/22.
-//
-
+//queues using linked list
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace::std;
+class Node{
+public:
+    int data;
+    Node* next;
+};
+class qll:protected Node{
+    Node* front=nullptr;
+    Node* rear=nullptr;
+public:
+    bool empty(){
+        if (front==nullptr) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    int remove(){
+        Node* p=new Node();
+        int x;
+        if (empty()) {
+            cout<<"queue is underflowed"<<endl;
+            exit(1);
+        }
+            p=front;
+            x= p->data;
+            front=p->next;
+        if (front==nullptr) {
+            rear=nullptr;
+        }
+        delete p;
+        return x;
+    }
+    void insert(int x){
+        Node *p=new Node();
+        p->data=x;
+        p->next=nullptr;
+        if (rear==nullptr) {
+            front=p;
+        } else{
+            rear->next=p;}
+            rear=p;
+    }
+};
+int main(){
+    qll l;
+    l.insert(4);
+    l.insert(5);
+    l.insert(6);
+    l.insert(7);
+    cout<<l.remove()<<endl;
+    cout<<l.remove()<<endl;
     return 0;
 }
